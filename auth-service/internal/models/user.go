28cfb38b822d1245dae21975dfd48a08717f0e18
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-10-22 18:44:06 +0530

    feat: initial setup and added user model

---
+++ auth-service/internal/models/user.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID           uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
+	Username     string    `gorm:"uniqueInde;not null" json:"username"`
+	Email        string    `gorm:"uniqueIndex;not null" json:"email"`
+	PasswordHash string    `gorm:"not null" json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type RegisterRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=50"`
+	Email    string `json:"email" validate:"requried,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type TokenResponse struct {
+	AccessToken  string  `json:"access_token"`
+	RefreshToken string  `json:"refresh_token"`
+	ExpiresIn    int64   `json:"expires_in"`
+	User         UserDTO `json:"user"`
+}
+
+type UserDTO struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dmehra2102/go-realtime-chat
+
+go 1.25.1
+
+require github.com/google/uuid v1.6.0
